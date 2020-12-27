c8dd4c775b2907b8087a4c7358c4aa0d03ae8c01
Pravat2003 <pravat2003@gmail.com>
2020-12-27 23:17:49 +0530

    commited after model changes

---
+++ RepositoryPattern/RepositoryPattern.Domain/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.Domain.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailId { get; set; }
+        public string MobileNo { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
---
+++ RepositoryPattern/RepositoryPattern.Infrastructure/Repositories/RoleRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RepositoryPattern.Domain.Models;
+
+namespace RepositoryPattern.Infrastructure.Repositories
+{
+    public class RoleRepository:Repository<Role>
+    {
+        public RoleRepository(SchoolContext context):base(context)
+        {
+
+        }
+        public override Role Add(Role entity)
+        {
+            return base.Add(entity);
+        }
+
+        public override IEnumerable<Role> GetAll()
+        {
+            return base.GetAll();
+        }
+
+        public override Role Remove(Role entity)
+        {
+            return base.Remove(entity);
+        }
+    }
+}
